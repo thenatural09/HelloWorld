f89672ba70b921b52481dc7e9ee2dc55a5b3f3e6
Troy <tward4@tulane.edu>
2016-09-13 14:38:00 -0400

    Human Class constructor and methods

--- src/Human.java
+++ src/Human.java
@@ -5,7 +5,50 @@
 public class Human {
     String name;
     String gender;
-    String height;
-    int weight;
     int age;
-}
+
+    static int minimumAge = 21;
+
+    //Constructor
+    Human(String newName, String newGender, int newAge){
+        setName(newName);
+        setGender(newGender);
+        setAge(newAge);
+    }
+
+
+    //Getter and Setter for Name
+    String getName(){
+        return name;
+    }
+
+    void setName(String newName){
+        name = newName;
+    }
+
+    //Getter and Setter for Gender
+    String getGender() {
+        return gender;
+    }
+
+    void setGender(String newGender) {
+        gender = newGender;
+
+    }
+
+    //Getter and Setter for Age
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int newAge) {
+        if(newAge >= minimumAge){
+            age = newAge;
+        }
+
+    }
+
+
+
+    }
+
