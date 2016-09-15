1424a268742f92b5d5bb447d582fa93b33786692
Troy <tward4@tulane.edu>
2016-09-15 11:19:57 -0400

    Dynamic data structures

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Troy on 9/14/16.
  */
@@ -20,12 +23,44 @@ public class Main {
             if (i != 1) {
                 System.out.println(names[i]);
             }
-
         }
 
         //new for loop
         for(String name : names) {
             System.out.println(name);
         }
+
+        //int -> Integer
+        //boolean --> Boolean
+
+
+        String number = "10";
+        int intNumber = Integer.valueOf(number);
+        System.out.println(intNumber);
+
+        //arraylists and hashmaps
+        ArrayList<String> names2 = new ArrayList();
+        names2.add("Alice");
+        names2.add("Bob");
+        names2.add("Charlie");
+
+        names2.remove(1);
+        System.out.println(names2);
+        //System.out.println(names2.get(0));
+
+
+        HashMap<String,Object> person = new HashMap();
+        person.put("name","Alice");
+        person.put("age",30);
+        person.put("location","Charleston");
+        System.out.println(person.get("name"));
+
+        //person.put("names", name2);
+        //names2.add(person);
+
+        String alice = names2.get(0);
+        String alice2 = (String) person.get("name");
+        System.out.println(alice);
+        System.out.println(alice2);
     }
 }
