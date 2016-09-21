54e22b17f3bad6fafd3b00a01713860581f43fb8
Troy <tward4@tulane.edu>
2016-09-21 10:28:02 -0400

    Removing from array lists and iterators

---
+++ src/Exercise03.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Created by Troy on 9/21/16.
+ */
+public class Exercise03 {
+    public static void main(String[] args) {
+        String[] names = {"alice", "bob", "charlie","daniel","andrew","alex"};
+        ArrayList<String> namesArr;
+
+
+        //for each
+        namesArr = new ArrayList<>(Arrays.asList(names));
+        for (String name : names) {
+//            String firstLetter = name.charAt(0);
+            if (name.charAt(0) == 'a') {
+                namesArr.remove(name);
+            }
+        }
+        System.out.println(namesArr);
+
+        //old school
+        namesArr = new ArrayList<>(Arrays.asList(names));
+        for (int i = namesArr.size() - 1; i >= 0; i--) {
+            if (namesArr.get(i).charAt(0) == 'a') {
+                namesArr.remove(i);
+            }
+        }
+        System.out.println(namesArr);
+
+        //iterators
+        namesArr = new ArrayList<>(Arrays.asList(names));
+        Iterator<String> iter = namesArr.iterator();
+        while (iter.hasNext()) {
+            String name = iter.next();
+            if (name.charAt(0) == 'a') {
+                iter.remove();
+            }
+        }
+        System.out.println(namesArr);
+    }
+}
